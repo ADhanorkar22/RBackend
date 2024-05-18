74e93fcae6f69b27961de9599f281f621aa60138
ADhanorkar22 <130759776+ADhanorkar22@users.noreply.github.com>
2024-05-18 13:54:03 +0530

    'ReadyForDemo'

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // // Customized CORS options (optional):
 const corsOptions = {
-  origin: 'https://www.rockkpay.com', // Allow requests from only from this specific origin
+  origin: 'https://rockkpay.com', // Allow requests from only from this specific origin
   methods: 'GET,POST,PUT,DELETE', // Allow only specified HTTP methods
   allowedHeaders: 'Content-Type,Authorization', // Allow only specified headers
   optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
